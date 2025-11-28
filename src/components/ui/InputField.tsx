f2a63bd9c4f47eaf44c334d10689ce226d625c33
sgc93 <smachewgedefaw@gmail.com>
2025-11-29 01:47:29 +0300

    fix: add required prop

--- src/components/ui/InputField.tsx
+++ src/components/ui/InputField.tsx
@@ -3,13 +3,15 @@ export const InputField = ({
   setValue,
   label,
   placeholder,
-  type = "text"
+  type = "text",
+  required = true
 }: {
   value: string;
   setValue: (v: string) => void;
   label: string;
   placeholder: string;
   type?: string;
+  required?: boolean;
 }) => {
   return (
     <div className="flex flex-col gap-2">
@@ -19,6 +21,7 @@ export const InputField = ({
         value={value}
         onChange={(e) => setValue(e.target.value)}
         placeholder={placeholder}
+        required={required}
         className="bg-n-500 px-2 py-2.5 text-n-50 font-semibold rounded-lg placeholder:text-n-100/50 text-sm transition-all duration-300 outline-none border border-n-400 focus:border focus:border-n-200"
       />
     </div>
