3ffe8fc35ab0967b4be6f7ea0cc4d649e1a23833
sgc93 <smachewgedefaw@gmail.com>
2025-11-28 07:57:17 +0300

    update: wrap the entire app with provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { useSelector } from "react-redux";
 import Dashboard from "./components/dashboard/Dashboard";
+import type { RootState } from "./redux/store";
+import Auth from "./components/auth/Auth";
 
 function App() {
+  const { user } = useSelector((state: RootState) => state.auth);
+  const isUserSignedIn = user !== null;
+
   return (
     <section className="w-screen h-screen overflow-hidden">
-      <Dashboard />
+      {isUserSignedIn ? <Dashboard /> : <Auth />}
     </section>
   );
 }
